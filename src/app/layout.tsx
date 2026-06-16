5ba109212c5e226e6fd945c3a546f76f7d518b55
Josh <josh.duncanson@gmail.com>
2026-06-16 10:01:01 -0400

    feat: make dark mode default regardless of OS preference

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body className={`${lato.variable} ${roboto.variable} antialiased`}>
         <SessionProvider>
           {children}
